3de9ddafc14829b34a48137528b29f6b8a9e9f3a
Qingsong Hu <kingsanhu@gmail.com>
2026-07-18 10:22:26 +0800

    Finish Step 1: Use React Navigation
    Add bottom tab navigator, and set custom color for icons

--- src/components/navigation/BadgerTabs.jsx
+++ src/components/navigation/BadgerTabs.jsx
@@ -1,9 +1,47 @@
 import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import BadgerNewsScreen from "../screens/BadgerNewsScreen";
+import BadgerPreferencesScreen from "../screens/BadgerPreferencesScreen";
+import { colorFill } from "ionicons/icons";
+
+const BadgerTabNavigator = createBottomTabNavigator();
 
 function BadgerTabs(props) {
-    return <>
-        <Text style={{paddingTop: 128}}>Hello World! I should make this tabs instead.</Text>
-    </>
+    return (
+        <BadgerTabNavigator.Navigator
+            screenOptions={({ route }) => ({
+                tabBarActiveTintColor: "red",
+                headerStyle: { backgroundColor: "lightblue" },
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === "News") {
+                        iconName = focused ? "newspaper" : "newspaper-outline";
+                    } else if (route.name === "Preferences") {
+                        iconName = focused ? "settings" : "settings-outline";
+                    }
+                    return (
+                        <Ionicons name={iconName} size={size} color={color} />
+                    );
+                },
+            })}
+        >
+            <BadgerTabNavigator.Screen
+                name="News"
+                component={BadgerNewsScreen}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <BadgerTabNavigator.Screen
+                name="Preferences"
+                component={BadgerPreferencesScreen}
+                options={{ headerShown: false }}
+            />
+        </BadgerTabNavigator.Navigator>
+    );
 }
 
 export default BadgerTabs;
