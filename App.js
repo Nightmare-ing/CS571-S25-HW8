7642ec08eedf14c6b13157ba635e8f1c137690ff
Qingsong Hu <kingsanhu@gmail.com>
2026-08-06 21:59:44 +0800

    Add SafeAreaProvider
    To avoid contents occupying the area of status bar

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-import BadgerNews from './src/components/BadgerNews';
+import BadgerNews from "./src/components/BadgerNews";
 
 export default function App() {
-  return (
-    <>
-      <BadgerNews/>
-    </>
-  );
+    return (
+        <>
+            <SafeAreaProvider>
+                <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
+                    <BadgerNews />
+                </SafeAreaView>
+            </SafeAreaProvider>
+        </>
+    );
 }
